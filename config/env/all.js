bce3d6bb72362ca0b2d553a864c717425b658230
root <root@ip-10-123-155-42.ec2.internal>
2014-10-20 10:24:17 -0400

    fixing

--- config/env/all.js
+++ config/env/all.js
@@ -5,7 +5,7 @@ var path = require('path'),
 
 module.exports = {
   root: rootPath,
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 80,
   hostname: process.env.HOST || process.env.HOSTNAME,
   db: process.env.MONGOHQ_URL,
   templateEngine: 'swig',
